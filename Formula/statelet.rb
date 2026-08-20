9e84f7cc9cfd4c16b927baffa2c88d3357d6bcd3
statelet-ci <ci@statelet.ai>
2026-08-20 18:17:42 +0000

    statelet 0.1.5

--- Formula/statelet.rb
+++ Formula/statelet.rb
@@ -1,26 +1,26 @@
 class Statelet < Formula
   desc "Distributed key-value storage engine with LSM-tree, Raft, and vector index"
   homepage "https://github.com/stateletlab/statelet"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.4/statelet-0.1.4-darwin-arm64.tar.gz"
-      sha256 "dc79789feeef63da9581d50ee3e2acefa08e45b1dce97aaa4967fc19d38fde9b"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.5/statelet-0.1.5-darwin-arm64.tar.gz"
+      sha256 "7573fef96a4bd76bf4e1b85d12bf68b80795090c42ac5ea98c2d95439062bf1a"
     else
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.4/statelet-0.1.4-darwin-amd64.tar.gz"
-      sha256 "56aff811b3edd0352be8ba1756d1146a3f8e4bf4fa93e33ea4c8d4a2248c693e"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.5/statelet-0.1.5-darwin-amd64.tar.gz"
+      sha256 "8120162d8f8f9e8616f0c865e9f4a443899f09f8f43c6e7ba99b56b674e84adc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.4/statelet-0.1.4-linux-arm64.tar.gz"
-      sha256 "85f57d805571825edaed5341c8e3392b3899af26a925b4ccb2fa3cdeb79fbb95"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.5/statelet-0.1.5-linux-arm64.tar.gz"
+      sha256 "07eff53d405ff011afbedc7e30d14c7059330b128efae682cb424df3c9c514bd"
     else
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.4/statelet-0.1.4-linux-amd64.tar.gz"
-      sha256 "e71429fd40a9ea98472330d10f8cd15ebecb7a56ed602ce410a0cced967cc658"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/v0.1.5/statelet-0.1.5-linux-amd64.tar.gz"
+      sha256 "f21d6ad66143dfaf4ba4daa44e66425e0cf92490c2308311ed1c247eba25e700"
     end
   end
 
