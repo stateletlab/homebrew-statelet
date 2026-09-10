20ecc995d2e67f73736e05819a5b58b399a31de5
statelet-ci <ci@statelet.ai>
2026-09-10 02:23:26 +0000

    statelet-lite 0.1.7

--- Formula/statelet-lite.rb
+++ Formula/statelet-lite.rb
@@ -1,26 +1,26 @@
 class StateletLite < Formula
   desc "Single-node Statelet database in one binary"
   homepage "https://github.com/stateletlab/statelet"
-  version "0.1.6"
+  version "0.1.7"
   license "FSL-1.1-ALv2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.6/statelet-lite-0.1.6-darwin-arm64.tar.gz"
-      sha256 "6f12b7c8c97d29cb81f1bd5cf64c98281964a998ba487fca176764de6231a915"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.7/statelet-lite-0.1.7-darwin-arm64.tar.gz"
+      sha256 "d3342c3f6ff62d43d830cbced0ba9eed0edb3dbe5fe5f984cbbd19dbb967134f"
     else
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.6/statelet-lite-0.1.6-darwin-amd64.tar.gz"
-      sha256 "ac7fdf7fab2a138eeb915145f318c5df4fbf4362af6ea6db56e389888c7582c6"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.7/statelet-lite-0.1.7-darwin-amd64.tar.gz"
+      sha256 "79819d735af26d70098e593584b3241926155c25ce8a7bdd7a358020dc6c5a85"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.6/statelet-lite-0.1.6-linux-arm64.tar.gz"
-      sha256 "4f39be4930427920369a87444cda5d1c5e848d7b7d6f03846afb886b3d01e7bd"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.7/statelet-lite-0.1.7-linux-arm64.tar.gz"
+      sha256 "fd9bb2b72750f4b7e7adbed82202010bdcb0756a8f08fd87fd9384543b3aa1a3"
     else
-      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.6/statelet-lite-0.1.6-linux-amd64.tar.gz"
-      sha256 "be495f02247c6dc5612ef138d45d310ae1ac804637243f5dbda396f7bceb0819"
+      url "https://github.com/stateletlab/statelet-longmemeval/releases/download/lite-v0.1.7/statelet-lite-0.1.7-linux-amd64.tar.gz"
+      sha256 "717053db565678ec496eb0027beb11d28899fd722a2c277f8f10c10a73eede72"
     end
   end
 
